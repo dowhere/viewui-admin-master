8fd79290246ac8e77e2ac392f593567c2c928e74
somken <somken@sohu.com>
2019-12-26 15:13:05 +0800

    vue-cli3+viewUI后台管理

---
+++ src/views/join-page.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Card shadow title="社区">
+      <row class="join-page" :gutter="32">
+        <i-col span="10">
+          <img class="qq-group-img" src="../assets/images/icon-qr-qq-wechat.png">
+          <row type="flex" justify="center">
+            <i-col span="12">
+              <p></p>
+            </i-col>
+            <i-col span="12"></i-col>
+          </row>
+        </i-col>
+        <i-col span="14">
+          <div class="join-page-other">
+            <Button to="https://zhuanlan.zhihu.com/feview" target="_blank" size="large">
+              <img src="../assets/images/icon-social-zhihu.svg" class="join-page-other-icon">
+              iView 知乎专栏
+            </Button>
+            
+          </div>
+        </i-col>
+      </row>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'join_page',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+  .join-page{
+    text-align: center;
+  }
+  .qq-group-img{
+    width: 100%;
+  }
+  .join-page-other-icon{
+    width: 20px;
+    vertical-align: middle;
+    margin-right: 6px;
+  }
+  .join-page-other{
+    text-align: left;
+  }
+  .join-page-other .ivu-btn{
+    margin-right: 6px;
+  }
+</style>
